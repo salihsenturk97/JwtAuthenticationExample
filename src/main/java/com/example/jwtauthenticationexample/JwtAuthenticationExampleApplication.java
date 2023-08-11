e65554c2d1bd1d2589a10e46f1481eb80267894c
salihsenturk97 <salihsenturk97@gmail.com>
2023-08-11 20:31:00 +0300

    First Commit

---
+++ src/main/java/com/example/jwtauthenticationexample/JwtAuthenticationExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtauthenticationexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthenticationExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jwtauthenticationexample/JwtAuthenticationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtauthenticationexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
